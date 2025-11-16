cf7272809d150184f460cfcd8ec3b2f3cbb33871
Steven Qiu <littleqiu@littleservice.cn>
2025-11-17 04:31:56 +0800

    optimize writing

--- src/Listeners/OnUserEmailVerificationSending.php
+++ src/Listeners/OnUserEmailVerificationSending.php
@@ -22,7 +22,7 @@ class OnUserEmailVerificationSending {
 	$this->filter->add('can_send_verification_email', function($can, $recipient) {
         if(Cache::get("oci-email-delivery-suppress-{$recipient}")) {
                 Log::channel('oci-email-delivery')->info('Email suppressed for ' . $recipient);
-                return new Rejection('你绑定的邮箱无法接收 LittleSkin 发送的邮件，请前往「个人资料」页面更改绑定邮箱后再尝试发送');
+                return new Rejection('你绑定的邮箱地址有误，请前往「个人资料」页面更改绑定邮箱后再尝试发送');
             }
             return $can;
         });
